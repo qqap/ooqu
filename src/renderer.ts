f67e7372e2be9583991f5b591e57bb87128c3f84
qqap <lavahumour@gmail.com>
2024-05-11 16:39:11 +0530

    patch

--- src/renderer.ts
+++ src/renderer.ts
@@ -28,9 +28,58 @@
 
 // import './index.css';
 import { persistentAtom } from '@nanostores/persistent'
-type LoadingStateValue = 'empty' | 'loading' | 'loaded'
+type SidePaneStateValue = 'open' | 'closed'
 
-const $loadingState = persistentAtom<LoadingStateValue>('empty')
-console.log($loadingState)
+const $loadingState = persistentAtom<SidePaneStateValue>('open')
+// console.log($loadingState)
+
+  
+const gutterdrag = document.querySelector<HTMLDivElement>('.gutter-col-1')
+const grid = document.querySelector<HTMLDivElement>('.grid')
+// console.log("gutterdrag", gutterdrag)
+
+function move(){
+
+    // transition hacks, need to be resolved.
+    setTimeout(() => {
+        // document.querySelector<HTMLDivElement>('.grid').style.transition = "300ms"
+        document.querySelector<HTMLDivElement>('.grid').style.gridTemplateColumns = "auto 2px 200px"
+          }, 200)
+
+ 
+}
+
+
+let lastClick = 0;
+gutterdrag.addEventListener('mousedown', (e) => {
+    // document.body.style.backgroundColor = 'red'
+
+
+    // console.log('click', e)
+  const thisClick = Date.now();
+  if (thisClick - lastClick < 400) {
+    console.log('quick click detected, returning early');
+    move()
+    lastClick = thisClick;
+    // return;
+  }
+  lastClick = thisClick;
+  console.log('click');
+});
+
+const pin = document.querySelector<HTMLDivElement>('#pin')
+console.log(pin)
+// global state store with nanostores.
+pin.addEventListener('click', () => {
+    console.log("clicked")
+    console.log(document.querySelector<HTMLDivElement>('.grid').style.gridTemplateAreas)
+    document.querySelector<HTMLDivElement>('.grid').style.gridTemplateColumns = "auto 2px 100px"
+})
+
+
+// gutterdrag.addEventListener('dblclick', () => {
+//         console.log("double clicked")
+//     document.querySelector<HTMLDivElement>('.grid').style.gridTemplateAreas = "auto 2px 200px"
+// })
 
 console.log('👋 This message is being logged by "renderer.ts", included via Vite');
